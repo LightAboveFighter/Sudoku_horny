ac236ec2b2890efbfe5e931bf1ac89222e59fb02
Alexander Kosarigin <123541292+LightAboveFighter@users.noreply.github.com>
2024-01-20 20:57:34 +0300

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/alexunder/Data/MIPT/C_projects/Sudoku_horny
-# Build directory: /media/alexunder/Data/MIPT/C_projects/Sudoku_horny/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
